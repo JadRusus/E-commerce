647878cf1af93bf5f3633aa43b155d4e89de54dd
JadRusus <amjad.arab7@gmail.com>
2023-03-29 00:05:04 +0300

    adding popup single product

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -2,10 +2,11 @@ import React, { useContext } from "react";
 import { ShopContext } from "../../context/shop-context";
 import { SingleProduct } from "./product";
 import { Categories } from "../../components/categories";
+import { AiFillCloseCircle } from "react-icons/ai";
 import "./shop.css";
 
 export const Shop = () => {
-  const { fulldata, selectCat, closePopup, popup, selectedId } =
+  const { fulldata, selectCat, closePopup, popup, selectedId, addToCart } =
     useContext(ShopContext);
 
   return (
@@ -33,12 +34,36 @@ export const Shop = () => {
                 style={{ top: Math.max(0, window.scrollY) }}
               >
                 <div className="popup-header">
-                  <h1>popup</h1>
-                  <h1 onClick={() => closePopup()}>x</h1>
+                  <h1>{fulldata[selectedId - 1].title}</h1>
+                  <h1 id="closebtn" onClick={() => closePopup()}>
+                    <AiFillCloseCircle size={32} />
+                  </h1>
                 </div>
-                <div>
-                  <p>{fulldata[selectedId - 1].id}</p>
-                  <p>{fulldata[selectedId - 1].title}</p>
+                <div className="popup-content">
+                  <div className="popup-content-left">
+                    <img
+                      src={fulldata[selectedId - 1].image}
+                      alt={fulldata[selectedId - 1].title}
+                    />
+                  </div>
+                  <div className="popup-content-right">
+                    <h3>{fulldata[selectedId - 1].title}</h3>
+
+                    <p>
+                      Rating: <b>{fulldata[selectedId - 1].rating.rate}</b> out
+                      of 5 | <b>{fulldata[selectedId - 1].rating.count}</b>{" "}
+                      Reviews
+                    </p>
+                    <h4>Price: ${fulldata[selectedId - 1].price}</h4>
+                    <h4>Products Details:</h4>
+                    <p>{fulldata[selectedId - 1].description}</p>
+                    <button
+                      className="addToCartBttn"
+                      onClick={() => addToCart(fulldata[selectedId - 1].id)}
+                    >
+                      Add to Cart
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
